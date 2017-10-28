99d071bb53eb9a49f3bfa3e11c451b81dd086f8d
CJVincent <christophvincent@gmail.com>
2017-10-28 16:18:44 -0700

    Post Test

--- routes/storeData.js
+++ routes/storeData.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds231245.mlab.com:31245/heroku_dh2zjfbf';
 
 router.post('/',urlencodedParser, function(req, res) {
