51a2b293680018fc951bbbefedcb973cde318c63
CJVincent <christophvincent@gmail.com>
2017-10-28 15:51:04 -0700

    Post Test

--- routes/storeData.js
+++ routes/storeData.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-var session = reqruie('express-session');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds231245.mlab.com:31245/heroku_dh2zjfbf';
