1e3860136f85164add83b89bf7aff4f52daa0a08
CJVincent <christophvincent@gmail.com>
2017-11-06 16:24:11 -0800

    version 1.0

--- routes/storeData.js
+++ routes/storeData.js
@@ -8,9 +8,12 @@ var jsonParser = bodyParser.json();
 var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds231245.mlab.com:31245/heroku_dh2zjfbf';
 router.post('/',jsonParser, function(req, res) {
     var requestBody = req.body;
-    console.log(requestBody);
+
     mongodb.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
+        if(err){
+            console.log(requestBody);
+            throw err;
+        }
 
         var Routes = db.collection('CUSTOMERS');
         var custID = new ObjectID();
@@ -60,7 +63,7 @@ router.post('/',jsonParser, function(req, res) {
         //only want code, quantity, and price in database
         var products = new Array();
         var totalCost = 0;
-        for (var i =0; i < requestBody.session_basket.length; i++)
+        for (var i =0; i < (requestBody.session_basket).length; i++)
         {
             var item = new Object();
             item.code = requestBody.session_basket[i].code;
