2c8a9f5f9e21df0c8362425949fb222cd4dd647e
CJVincent <christophvincent@gmail.com>
2017-11-08 12:18:45 -0800

    version 1.1

--- routes/storeData.js
+++ routes/storeData.js
@@ -2,18 +2,13 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
-var jsonParser = bodyParser.json();
 var mongoDBURI = process.env.MONGODB_URI ||'mongodb://CJV:doritos61@ds231245.mlab.com:31245/heroku_dh2zjfbf';
-router.post('/',jsonParser, function(req, res) {
+router.post('/', function(req, res) {
     var requestBody = req.body;
-    console.log(requestBody);
+    //console.log(requestBody);
     mongodb.connect(mongoDBURI, function(err, db) {
         if(err) throw err;
-
-
-        var Routes = db.collection('CUSTOMERS');
+        var dbColl = db.collection('CUSTOMERS');
         var custID = new ObjectID();
         var cust = {
             _id: custID,
@@ -25,11 +20,9 @@ router.post('/',jsonParser, function(req, res) {
             ZIP: requestBody.zipB,
             EMAIL: requestBody.emailB
         };
-        Routes.insertOne(cust, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-        });
-        Routes = db.collection('BILLING');
+
+        dbColl.insertOne(cust,processRequest);
+        dbColl = db.collection('BILLING');
         var billID = new ObjectID();
         var bill = {
             _id: billID,
@@ -39,12 +32,9 @@ router.post('/',jsonParser, function(req, res) {
             CREDITCARDEXP: requestBody.cardExpiration,
             CREDITCARDDATE: requestBody.cardExpiration,
             CREDITCARDSECURITYNUM: requestBody.cardSecurityNumber
-        }
-        Routes.insertOne(bill, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-        });
-        Routes = db.collection('SHIPPING');
+        };
+        dbColl.insertOne(bill,processRequest);
+        dbColl = db.collection('SHIPPING');
         var shipID = new ObjectID();
         var ship = {
             _id: shipID,
@@ -52,27 +42,22 @@ router.post('/',jsonParser, function(req, res) {
             SHIPPING_CITY: requestBody.city,
             SHIPPING_STATE: requestBody.state,
             SHIPPING_ZIP: requestBody.zip
-        }
-        Routes.insertOne(ship, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-        });
-        Routes = db.collection('ORDERS');
+        };
+        dbColl.insertOne(ship,processRequest);
+        dbColl = db.collection('ORDERS');
         //only want code, quantity, and price in database
         var products = new Array();
         var totalCost = 0;
-        for (var i =0; i < requestBody.numItems; i++)
+        for (var i =0; i < requestBody.session_basket.length; i++)
         {
             var item = new Object();
             item.code = requestBody.session_basket[i].code;
             item.quantity = requestBody.session_basket[i].quantity;
             item.price = requestBody.session_basket[i].price;
             totalCost += parseFloat(item.price * item.quantity);
-            console.log('totalCost so far ' + totalCost);
             item = JSON.stringify(item);
             products.push(item);
         }
-        console.log(totalCost);
         var order = {
             CUSTOMER_ID: custID,
             BILLING_ID: billID,
@@ -80,11 +65,8 @@ router.post('/',jsonParser, function(req, res) {
             DATE: new Date(),
             PRODUCT_VECTOR: products,
             ORDER_TOTAL: ((totalCost +(totalCost*0.08)) +2).toFixed(2)
-        }
-        Routes.insertOne(order, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-        });
+        };
+        dbColl.insertOne(order,processRequest);
         //close connection when your app is terminating.
         db.close(function (err) {
             if(err) throw err;
@@ -93,4 +75,11 @@ router.post('/',jsonParser, function(req, res) {
     });//end of connect
     res.render('storeData', { data: requestBody});
 });
+
+function processRequest(err, res)
+{
+    if (err) throw err;
+    console.log("1 document inserted");
+    return res;
+}
 module.exports = router;
